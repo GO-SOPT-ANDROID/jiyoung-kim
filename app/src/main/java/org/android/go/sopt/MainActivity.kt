b996b808f75273f8acd215d69b05c1e450f679f5
Cat-JiYoung <kimyamae82@gmail.com>
2023-04-04 02:25:24 +0900

    feat(signIn): 로그인 화면 구현

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.android.go.sopt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMainLogin.setOnClickListener {
+            Snackbar.make(
+                binding.root,
+                "안녕하세영",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 }
