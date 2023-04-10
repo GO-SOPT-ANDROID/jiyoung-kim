9ee289a4d42232162e09bc4aabca174c80e04827
Cat-JiYoung <kimyamae82@gmail.com>
2023-04-10 20:06:04 +0900

    feat(home): recyclerView image 추가

--- app/src/main/java/org/android/go/sopt/data/model/Repo.kt
+++ app/src/main/java/org/android/go/sopt/data/model/Repo.kt
@@ -1,6 +1,9 @@
 package org.android.go.sopt.data.model
 
+import androidx.annotation.DrawableRes
+
 data class Repo(
+    @DrawableRes val image: Int,
     val name: String,
     val repoName: String
 )
