234bc94b055edb09528f9ebbbbb3f3ec10dc6b2f
Cat-JiYoung <kimyamae82@gmail.com>
2023-04-13 23:29:15 +0900

    feat(home): 불필요한 코드 제거

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -1,7 +1,6 @@
 package org.android.go.sopt
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import org.android.go.sopt.databinding.ActivityMainBinding
@@ -16,30 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         clickNavigationBtn()
-//        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
-//        if (currentFragment == null) {
-//            supportFragmentManager.beginTransaction()
-//                .add(R.id.fcv_main, HomeFragment())
-//                .commit()
-//        }
-        supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
+//        supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
     }
 
     private fun clickNavigationBtn() {
         binding.bnvHome.setOnItemSelectedListener { item ->
-            Log.d("main", "clickNavigationBtn() called")
             changeFragment(
                 when (item.itemId) {
                     R.id.menu_home -> {
-                        Log.d("main", "changeFragment() 호출 fragment = ${item.itemId}")
                         HomeFragment()
                     }
                     R.id.menu_gallery -> {
-                        Log.d("main", "changeFragment() 호출 fragment = ${item.itemId}")
                         GalleryFragment()
                     }
                     else -> {
-                        Log.d("main", "changeFragment() 호출 fragment = ${item.itemId}")
                         SearchFragment()
                     }
                 }
