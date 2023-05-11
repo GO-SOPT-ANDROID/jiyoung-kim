c5c968e79853502633e9abb2662f2b05963ad3af
Cat-JiYoung <kimyamae82@gmail.com>
2023-05-11 11:45:11 +0900

    feat(auth): 로그인 Dto 추가

---
+++ app/src/main/java/org/android/go/sopt/data/model/response/ResponseSignInDto.kt
@@ -0,0 +1,24 @@
+package org.android.go.sopt.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseSignInDto(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: SignInData
+) {
+    @Serializable
+    data class SignInData(
+        @SerialName("id")
+        val id: String,
+        @SerialName("name")
+        val name: String,
+        @SerialName("skill")
+        val skill: String
+    )
+}
