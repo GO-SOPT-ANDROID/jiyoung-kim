a92e8d22b6162be74c72fee97bd0e203020a47e9
Cat-JiYoung <kimyamae82@gmail.com>
2023-05-11 11:54:41 +0900

    feat(AuthRemoteDataSource): signUp() return type 수정

--- app/src/main/java/org/android/go/sopt/data/datasource/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/org/android/go/sopt/data/datasource/remote/AuthRemoteDataSource.kt
@@ -11,7 +11,7 @@ import retrofit2.Call
 서버와 직접 통신하여 data를 관리하는 data source
  */
 class AuthRemoteDataSource(private val authService: AuthService) {
-    fun signUp(signUpRequest: RequestSignUpDto): Call<ResponseSignUpDto> =
+    fun signUp(signUpRequest: RequestSignUpDto): ResponseSignUpDto =
         authService.signUp(signUpRequest)
 
     fun signIn(signInResult: RequestSignInDto): Call<ResponseSignInDto> =
