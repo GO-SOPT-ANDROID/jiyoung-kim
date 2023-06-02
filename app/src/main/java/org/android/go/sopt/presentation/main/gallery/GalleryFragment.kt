b81287e6755bd0966c37d33759a6bbc3adb802df
Cat-JiYoung <kimyamae82@gmail.com>
2023-06-02 16:56:46 +0900

    feat(gallery): 로딩뷰 구현

--- app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentGalleryBinding
 import org.android.go.sopt.util.BindingFragment
+import org.android.go.sopt.util.LoadingDialog
 
 @AndroidEntryPoint
 class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
@@ -26,9 +31,19 @@ class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragmen
     }
 
     private fun setAdapter(listAdapter: GalleryMemberListAdapter) {
+        val dialog = LoadingDialog(requireContext())
+        dialog.show()
         viewModel.soptMembers.observe(viewLifecycleOwner) {
-            binding.rcvGalleryView.adapter = listAdapter
-            listAdapter.submitList(it)
+            if (it.isEmpty()) {
+                CoroutineScope(Main).launch {
+                    delay(9000)
+                    dialog.dismiss()
+                }
+            } else {
+                dialog.dismiss()
+                binding.rcvGalleryView.adapter = listAdapter
+                listAdapter.submitList(it)
+            }
         }
     }
 }
---
+++ app/src/main/java/org/android/go/sopt/util/LoadingDialog.kt
@@ -0,0 +1,22 @@
+package org.android.go.sopt.util
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import org.android.go.sopt.R
+
+class LoadingDialog
+constructor(context: Context) : Dialog(context) {
+
+    init {
+        setCanceledOnTouchOutside(false)
+        setCancelable(false)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.dialog_loading, null)
+        setContentView(view)
+    }
+}
