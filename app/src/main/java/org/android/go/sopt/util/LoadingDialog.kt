37344aaf43ce6dd42493d3220e68711b11f5fbf4
Cat-JiYoung <kimyamae82@gmail.com>
2023-06-14 18:29:57 +0900

    feat(util): dialog -> dialog fragment로 변경

--- app/src/main/java/org/android/go/sopt/util/LoadingDialog.kt
+++ app/src/main/java/org/android/go/sopt/util/LoadingDialog.kt
@@ -1,12 +1,15 @@
 package org.android.go.sopt.util
 
-import android.app.Dialog
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import android.view.LayoutInflater
-import org.android.go.sopt.R
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.android.go.sopt.databinding.DialogLoadingBinding
 
+/*
 class LoadingDialog(context: Context) : Dialog(context) {
 
     init {
@@ -17,5 +20,33 @@ class LoadingDialog(context: Context) : Dialog(context) {
         val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(R.layout.dialog_loading, null)
         setContentView(view)
+    }
+}
+
+ */
+class LoadingDialog : DialogFragment() {
+    private var _binding: DialogLoadingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogLoadingBinding.inflate(inflater, container, false)
+        val view = binding.root
+        // 레이아웃 배경을 투명하게 해줌, 필수 아님
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+//        binding.dialBtn1.setOnClickListener {
+//            dismiss()    // 대화상자를 닫는 함수
+//        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
